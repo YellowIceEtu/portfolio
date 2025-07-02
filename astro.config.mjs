4d8cc1253c9d7be3e721ef328072c55fefd491c0
François <YellowIce_13009@Outlook.com>
2025-07-02 16:24:24 +0200

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site:'https://YellowIceEtu.github.io',
+    base:'portfolio',
+    trailingSlash: 'always'
+});
